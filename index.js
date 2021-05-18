97d6f9ba726c01c89734fb92eb9fb2854b562806
Hamna Kamran <74135890+hamnakamran10@users.noreply.github.com>
2021-05-18 08:58:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//LOOPS
+//Loops have two types
+//*for loops *while loops
+ let num=0;
+// while(num < 20){
+//     num++;
+//     console.log(num)
+// }
+
+
+// while(num < 20){
+//     num=+2;
+//     console.log(num)
+// }
+
+// while(num < 30){
+//     num=+3;
+//     console.log(num)
+// }
+
+// while(num < 40){
+//     num=+4;
+//     console.log(num)
+// }
+
+// while(num < 50){
+//     num=+5;
+//     console.log(num)
+// }
+
+let numbers= parseInt(prompt("Enter No"))
+for (let num=1; num<11; num++){
+    let result=numbers*num;
+    console.log(result);
+}
+
+
+
+
